0269eaa69a8691ba185759a39e5589846d858d25
Sergei Anisimov <s.anisimov.it@gmail.com>
2026-09-15 10:55:49 +0200

    Bugfix

--- Assets/Scripts/GUI/Sliders/Slider.cs
+++ Assets/Scripts/GUI/Sliders/Slider.cs
@@ -1,5 +1,4 @@
-﻿using Unity.Mathematics.Geometry;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 
 namespace GUI.Sliders
@@ -7,17 +6,29 @@ namespace GUI.Sliders
   public class Slider : MonoBehaviour
   {
     [SerializeField] protected AudioMixerGroup _mixerSlider;
-    [SerializeField] protected float _minVolume;
-    [SerializeField] protected float _maxVolume;
     [SerializeField] protected string _mixerGroupName;
-    
-    private float _currentVolume;
+
+    private const float _minLinearVolume = 0.0001f;
+    private const float _minDb = -80f;
+
+    private float _currentVolume = -1f;
     private const float _epsilon = 0.001f;
+
+    private void Start()
+    {
+      float saved = PlayerPrefs.GetFloat(_mixerGroupName, 1f);
+      ChangeVolume(saved);
+    }
+
     public void ChangeVolume(float volume)
     {
       if (VolumesAreEqual(volume)) return;
-      
-      _mixerSlider.audioMixer.SetFloat(_mixerGroupName, Mathf.Lerp(_minVolume, _maxVolume, Mathf.Log10(volume) * 20f));
+
+      float dB = volume <= _minLinearVolume
+        ? _minDb
+        : Mathf.Log10(volume) * 20f;
+
+      _mixerSlider.audioMixer.SetFloat(_mixerGroupName, dB);
       _currentVolume = volume;
       PlayerPrefs.SetFloat(_mixerGroupName, volume);
     }
